979187cb9410253ff1fb5cd67bead26d95432c15
srugunda <srugunda@gmail.com>
2020-07-19 22:06:46 +0300

    guess the tv show_update

--- app/src/main/java/com/example/guessthetvshow/MainActivity.java
+++ app/src/main/java/com/example/guessthetvshow/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    public void playGame(View view) {
         Intent intent = new Intent(MainActivity.this, ImagesActivity.class);
         startActivity(intent);
     }
